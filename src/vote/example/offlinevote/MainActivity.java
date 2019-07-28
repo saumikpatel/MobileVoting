8d3c93382380691968ada9c911ae54f805b5a32f
saumik patel <saumikpatel32@gmail.com>
2019-07-27 21:21:34 -0400

    Initial project Strucuture

---
+++ src/vote/example/offlinevote/MainActivity.java
@@ -0,0 +1,34 @@
+package vote.example.offlinevote;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends Activity {
+  
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       
+        
+        
+       
+        
+        
+       
+    }
+
+   
+
+   
+}
