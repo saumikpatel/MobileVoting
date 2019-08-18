60117d66e2290ab07a797c7e1cc50698f846deca
saumik patel <parthiv997888@gmail.com>
2019-08-17 20:25:47 -0400

    thank you for voting UI

--- src/vote/example/offlinevote/R.java
+++ src/vote/example/offlinevote/R.java
@@ -11,56 +11,88 @@ public final class R {
     }
 
     public static final class drawable {
-       
+        public static final int add = 2130837504;
         public static final int ic_launcher = 2130837505;
-     
+        public static final int reset = 2130837506;
+        public static final int sleader = 2130837507;
+        public static final int vote = 2130837508;
+        public static final int votes = 2130837509;
+        public static final int white = 2130837510;
     }
 
     public static final class id {
         public static final int action_settings = 2131230742;
-		public static final int b1 = 2131230722;
+        public static final int admin = 2131230739;
+        public static final int b1 = 2131230722;
         public static final int b2 = 2131230726;
-		public static final int et1 = 2131230724;
-        public static final int et2 = 2131230725;
-		 public static final int et3 = 2131230734;
-        public static final int et4 = 2131230735;
-		public static final int b3 = 2131230736;
+        public static final int b3 = 2131230736;
         public static final int b5 = 2131230737;
-		    public static final int t1 = 2131230733;
-			 public static final int l1 = 2131230723;
-			  public static final int e = 2131230727;
+        public static final int e = 2131230727;
         public static final int e1 = 2131230720;
-		       public static final int rg1 = 2131230741;
-
+        public static final int e2 = 2131230721;
+        public static final int e3 = 2131230732;
+        public static final int et1 = 2131230724;
+        public static final int et2 = 2131230725;
+        public static final int et3 = 2131230734;
+        public static final int et4 = 2131230735;
+        public static final int exit = 2131230740;
+        public static final int home = 2131230738;
+        public static final int i1 = 2131230728;
+        public static final int i2 = 2131230729;
+        public static final int i3 = 2131230730;
+        public static final int i4 = 2131230731;
+        public static final int l1 = 2131230723;
+        public static final int rg1 = 2131230741;
+        public static final int t1 = 2131230733;
     }
 
     public static final class layout {
-
+        public static final int activity_adduser = 2130903040;
+        public static final int activity_elector = 2130903041;
+        public static final int activity_input = 2130903042;
         public static final int activity_main = 2130903043;
-		public static final int admin = 2130903048;
-		public static final int activity_signup = 2130903046;
-		  public static final int vote = 2130903051;
-		  public static final int activity_reset_password = 2130903044;
-   
+        public static final int activity_reset_password = 2130903044;
+        public static final int activity_setting = 2130903045;
+        public static final int activity_signup = 2130903046;
+        public static final int activityresetuser = 2130903047;
+        public static final int admin = 2130903048;
+        public static final int count = 2130903049;
+        public static final int end = 2130903050;
+        public static final int vote = 2130903051;
     }
 
     public static final class menu {
-     
+        public static final int adduser = 2131165184;
+        public static final int admin = 2131165185;
+        public static final int count = 2131165186;
+        public static final int elector = 2131165187;
+        public static final int end = 2131165188;
+        public static final int input = 2131165189;
         public static final int main = 2131165190;
-		 public static final int admin = 2131165185;
-		  public static final int reset_password = 2131165192;
-       public static final int signup = 2131165195;
-	        public static final int vote = 2131165196;
+        public static final int party = 2131165191;
+        public static final int reset_password = 2131165192;
+        public static final int resetuser = 2131165193;
+        public static final int setting = 2131165194;
+        public static final int signup = 2131165195;
+        public static final int vote = 2131165196;
     }
 
     public static final class string {
         public static final int action_settings = 2131034113;
         public static final int app_name = 2131034112;
         public static final int hello_world = 2131034114;
-		  public static final int title_activity_admin = 2131034115;
-		   public static final int title_activity_signup = 2131034126;
-		      public static final int title_activity_vote = 2131034116;
-      public static final int title_activity_reset_password = 2131034125; 
+        public static final int title_activity_adduser = 2131034120;
+        public static final int title_activity_admin = 2131034115;
+        public static final int title_activity_count = 2131034118;
+        public static final int title_activity_elector = 2131034123;
+        public static final int title_activity_end = 2131034117;
+        public static final int title_activity_input = 2131034122;
+        public static final int title_activity_party = 2131034121;
+        public static final int title_activity_reset_password = 2131034125;
+        public static final int title_activity_resetuser = 2131034119;
+        public static final int title_activity_setting = 2131034124;
+        public static final int title_activity_signup = 2131034126;
+        public static final int title_activity_vote = 2131034116;
     }
 
     public static final class style {
